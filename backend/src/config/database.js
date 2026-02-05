cf223314377ed710c78e39456476515ed237d5a9
HoneyyNagpal <honeyynagpal11@gmail.com>
2026-02-05 08:35:40 +0530

    add SSL config for production database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+  dialect: 'postgresql',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   pool: {
     max: 5,
     min: 0,
